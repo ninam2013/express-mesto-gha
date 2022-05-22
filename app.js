24d581ae09ca5723a7e647b065f55fe883040a4a
unknown <ninam2013@gmail.com>
2022-05-22 12:59:17 +0500

    Prescribed mistakes

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const cards = require('./routes/cards');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
 // обязательно должно быть!!! без этого не работает
 app.use(express.json());
 
 // заглушка
-app.use((req, res, next) => {
+app.use((req, _, next) => {
   req.user = {
     // здесь _id созданного пользователя
     _id: '62868b1326c4e504695519fa',
